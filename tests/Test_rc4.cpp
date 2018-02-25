aea7f136e96171c0f0f2bae375c8444b0c1efe1a
Alexander Elfimow <my.free.mail@gmx.de>
2018-02-25 21:31:46 +0100

    Added class

---
+++ tests/Test_rc4.cpp
@@ -0,0 +1,24 @@
+#include <rc4func.h>
+
+#include "Test_rc4.h"
+
+
+Test_rc4::Test_rc4(std::vector<uint8_t> &key) :
+    sbox { }
+{
+    sbox.reserve(256);
+    rc4init(key.data(), key.size(), sbox.data());
+}
+
+Test_rc4::~Test_rc4()
+{
+}
+
+std::vector<uint8_t> Test_rc4::run(const std::vector<uint8_t> &data)
+{
+    std::vector<uint8_t> result(data.size());
+
+    rc4run(data.data(), data.size(), result.data(), sbox.data());
+
+    return result;
+}
---
+++ tests/Test_rc4.h
@@ -0,0 +1,28 @@
+#ifdef TEST_RC4_H
+#error Already included
+#else
+#define TEST_RC4_H
+
+#include <cstdint>
+#include <vector>
+
+class Test_rc4
+{
+    public:
+        Test_rc4(std::vector<uint8_t> &key);
+        ~Test_rc4();
+
+        std::vector<uint8_t> run(const std::vector<uint8_t> &data);
+
+    private:
+        std::vector<uint8_t> sbox;
+
+    public:
+        Test_rc4() = delete;
+        Test_rc4(const Test_rc4 &instance) = delete;
+        Test_rc4(const Test_rc4 &&instance) = delete;
+        Test_rc4 &operator=(const Test_rc4 &instance) = delete;
+        Test_rc4 &operator=(const Test_rc4 &&instance) = delete;
+};
+
+#endif
