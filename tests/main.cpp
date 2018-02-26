90e04d50fca5728909255bc740e9dd8e86b25716
Alexander Elfimow <my.free.mail@gmx.de>
2018-02-26 21:09:41 +0100

    Extended test

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,20 +1,39 @@
 #include <iostream>
 
 #include "Golden_rc4.h"
+#include "Test_rc4.h"
 
 
 static_assert(sizeof(size_t) == 8, "size_t expected to be 64 bit");
 
 
-template <typename T> void check_size(const T &v1, const T &v2)
+template <typename T1, typename T2, typename T3> void run_test(const T1 &key, const T2 &data, T3 count)
 {
-    bool isEqual { v1.size() == v2.size() };
+    Golden_rc4 grc4 { key };
+    auto grc4_enc = grc4.run(data);
 
-    if (!isEqual)
+    Test_rc4 trc4 { key };
+    auto trc4_enc = trc4.run(data);
+
+    if (grc4_enc == data)
     {
-        throw std::logic_error("size check failed");
+        std::cout << "run_test count = " << count << std::endl;
+        throw std::logic_error("grc4_enc == data");
+    }
+
+    if (trc4_enc == data)
+    {
+        std::cout << "run_test count = " << count << std::endl;
+        throw std::logic_error("trc4_enc == data");
+    }
+
+    if (grc4_enc != trc4_enc)
+    {
+        std::cout << "run_test count = " << count << std::endl;
+        throw std::logic_error("grc4_enc != trc4_enc");
     }
 }
+
 
 int main(int argc, char *argv[])
 try
@@ -22,12 +41,19 @@ try
     argc = argc;
     argv = argv;
 
-    std::vector<uint8_t> key { 1, 2, 3, 4, 5 };
-    Golden_rc4 grc4 { key };
+    {
+        std::vector<uint8_t> key { 0 };
+        std::vector<uint8_t> data { 0 };
 
-    const std::vector<uint8_t> data { 0, 0, 0, 0 };
-    auto result = grc4.run(data);
-    check_size(data, result);
+        auto count { 0 };
+
+        while (count < 1000)
+        {
+            run_test(key, data, count);
+            data.push_back(0);
+            ++count;
+        }
+    }
 
     return EXIT_SUCCESS;
 }
